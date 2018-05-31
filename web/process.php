8daaf21e4725f0930fe3ad2584550de018daf197
nzsquirrell <squirrell@nutty.one>
2018-05-31 19:21:41 +1200

    first commit

---
+++ web/process.php
@@ -0,0 +1,14 @@
+<?php
+$to = "teamunitus@gmail.com"; 
+$from = $_REQUEST['email']; 
+$name = $_REQUEST['name']; 
+$headers = "From: $from"; 
+$subject = "You have a message sent from your site"; 
+$fields = array(); 
+$fields{"name"} = "name"; 
+$fields{"email"} = "email"; 
+$fields{"topic"} = "topic"; 
+$fields{"message"} = "message";
+$body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+$send = mail($to, $subject, $body, $headers);
+?>
